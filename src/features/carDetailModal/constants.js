bb7bcd9356a6ad17eaba78a14c66229108b1f27f
Burit S <burit3594@gmail.com>
2024-10-21 03:19:39 +0700

    :lipstick: [FEB] Fix Layout

---
+++ src/features/carDetailModal/constants.js
@@ -0,0 +1,219 @@
+export const CARDETAIL_TABLE_HEADER = [
+    "คันที่",
+    "เลขทะเบียนรถ",
+    "วันที่",
+    "เวลาเข้า",
+    "เวลาออก",
+    "ระยะเวลาจอด",
+    "ค่าบริการ (บาท)",
+    "หมายเหตุ",
+  ];
+  
+  export const MOCK_CARDETAIL_TABLE = [
+    {
+        id: 1,
+        licensePlate: "กข 1234",
+        date: "2024-10-20",
+        checkInTime: "08:00",
+        checkOutTime: "10:00",
+        duration: "2 ชั่วโมง",
+        serviceFee: 20,
+        remarks: "ไม่มี"
+    },
+    {
+        id: 2,
+        licensePlate: "ขค 5678",
+        date: "2024-10-20",
+        checkInTime: "09:30",
+        checkOutTime: "11:30",
+        duration: "2 ชั่วโมง",
+        serviceFee: 20,
+        remarks: "ไม่มี"
+    },
+    {
+        id: 3,
+        licensePlate: "คก 9012",
+        date: "2024-10-20",
+        checkInTime: "07:45",
+        checkOutTime: "12:00",
+        duration: "4 ชั่วโมง 15 นาที",
+        serviceFee: 50,
+        remarks: "ไม่มี"
+    },
+    {
+        id: 4,
+        licensePlate: "ง2 3456",
+        date: "2024-10-20",
+        checkInTime: "11:00",
+        checkOutTime: "13:00",
+        duration: "2 ชั่วโมง",
+        serviceFee: 20,
+        remarks: "ไม่มี"
+    },
+    {
+        id: 5,
+        licensePlate: "จจ 7890",
+        date: "2024-10-20",
+        checkInTime: "08:15",
+        checkOutTime: "09:45",
+        duration: "1 ชั่วโมง 30 นาที",
+        serviceFee: 15,
+        remarks: "ไม่มี"
+    },
+    {
+        id: 6,
+        licensePlate: "ซซ 1122",
+        date: "2024-10-19",
+        checkInTime: "10:00",
+        checkOutTime: "14:00",
+        duration: "4 ชั่วโมง",
+        serviceFee: 40,
+        remarks: "ไม่มี"
+    },
+    {
+        id: 7,
+        licensePlate: "ชช 3344",
+        date: "2024-10-19",
+        checkInTime: "12:00",
+        checkOutTime: "16:00",
+        duration: "4 ชั่วโมง",
+        serviceFee: 40,
+        remarks: "ไม่มี"
+    },
+    {
+        id: 8,
+        licensePlate: "ฆก 5566",
+        date: "2024-10-19",
+        checkInTime: "09:00",
+        checkOutTime: "10:30",
+        duration: "1 ชั่วโมง 30 นาที",
+        serviceFee: 15,
+        remarks: "ไม่มี"
+    },
+    {
+        id: 9,
+        licensePlate: "ฑฑ 7788",
+        date: "2024-10-18",
+        checkInTime: "13:00",
+        checkOutTime: "15:00",
+        duration: "2 ชั่วโมง",
+        serviceFee: 20,
+        remarks: "ไม่มี"
+    },
+    {
+        id: 10,
+        licensePlate: "ฒฒ 9900",
+        date: "2024-10-18",
+        checkInTime: "14:00",
+        checkOutTime: "16:30",
+        duration: "2 ชั่วโมง 30 นาที",
+        serviceFee: 25,
+        remarks: "ไม่มี"
+    },
+    {
+        id: 11,
+        licensePlate: "เฒเฒ 1234",
+        date: "2024-10-17",
+        checkInTime: "08:00",
+        checkOutTime: "09:00",
+        duration: "1 ชั่วโมง",
+        serviceFee: 10,
+        remarks: "ไม่มี"
+    },
+    {
+        id: 12,
+        licensePlate: "ฟฟ 5678",
+        date: "2024-10-17",
+        checkInTime: "11:00",
+        checkOutTime: "13:00",
+        duration: "2 ชั่วโมง",
+        serviceFee: 20,
+        remarks: "ไม่มี"
+    },
+    {
+        id: 13,
+        licensePlate: "มม 9012",
+        date: "2024-10-17",
+        checkInTime: "15:00",
+        checkOutTime: "17:00",
+        duration: "2 ชั่วโมง",
+        serviceFee: 20,
+        remarks: "ไม่มี"
+    },
+    {
+        id: 14,
+        licensePlate: "ยย 3456",
+        date: "2024-10-16",
+        checkInTime: "09:30",
+        checkOutTime: "12:00",
+        duration: "2 ชั่วโมง 30 นาที",
+        serviceFee: 25,
+        remarks: "ไม่มี"
+    },
+    {
+        id: 15,
+        licensePlate: "ร 7890",
+        date: "2024-10-16",
+        checkInTime: "10:00",
+        checkOutTime: "11:00",
+        duration: "1 ชั่วโมง",
+        serviceFee: 10,
+        remarks: "ไม่มี"
+    },
+    {
+        id: 16,
+        licensePlate: "ลล 1122",
+        date: "2024-10-15",
+        checkInTime: "13:00",
+        checkOutTime: "15:00",
+        duration: "2 ชั่วโมง",
+        serviceFee: 20,
+        remarks: "ไม่มี"
+    },
+    {
+        id: 17,
+        licensePlate: "วว 3344",
+        date: "2024-10-15",
+        checkInTime: "14:30",
+        checkOutTime: "16:00",
+        duration: "1 ชั่วโมง 30 นาที",
+        serviceFee: 15,
+        remarks: "ไม่มี"
+    },
+    {
+        id: 18,
+        licensePlate: "สส 5566",
+        date: "2024-10-14",
+        checkInTime: "09:00",
+        checkOutTime: "12:00",
+        duration: "3 ชั่วโมง",
+        serviceFee: 30,
+        remarks: "ไม่มี"
+    },
+    {
+        id: 19,
+        licensePlate: "สว 7788",
+        date: "2024-10-14",
+        checkInTime: "11:30",
+        checkOutTime: "13:00",
+        duration: "1 ชั่วโมง 30 นาที",
+        serviceFee: 15,
+        remarks: "ไม่มี"
+    },
+    {
+        id: 20,
+        licensePlate: "สา 9900",
+        date: "2024-10-14",
+        checkInTime: "12:00",
+        checkOutTime: "14:00",
+        duration: "2 ชั่วโมง",
+        serviceFee: 20,
+        remarks: "ไม่มี"
+    }
+];
+
+
+export const MOCK_DATA_API = {
+    pageSize: 10,
+    rows: 508,
+};
