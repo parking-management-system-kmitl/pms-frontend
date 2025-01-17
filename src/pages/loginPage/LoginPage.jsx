b3a7debfb22c7feb3402fca4391433ccc368f36e
64010850 Siwakorn Suriya <siwakornsuriya7856@gmail.com>
2025-01-17 15:08:27 +0700

    เชื่อม API
    Login
    สมัครสมาชิกVIP

--- src/pages/loginPage/LoginPage.jsx
+++ src/pages/loginPage/LoginPage.jsx
@@ -1,18 +1,70 @@
-import React, { useEffect } from "react";
+import React, { useEffect ,useState } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useNavigate } from "react-router-dom";
 
 function LoginPage() {
+  const apiUrl = process.env.REACT_APP_API_URL;
+
   const navigate = useNavigate();
 
   useEffect(() => {
     AOS.init({ duration: 1000 });
   }, []);
 
-  const handleSignIn = () => {
-    navigate("/detail");
+  // const handleSignIn = () => {
+  //   navigate("/detail");
+  // };
+
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prev => ({
+      ...prev,
+      [name]: value
+    }));
   };
+
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+    try {
+      console.log("start login ");
+      const response = await fetch(`${apiUrl}/auth/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData)
+      });
+      
+      // แสดงข้อมูล response ทั้งหมด
+      const data = await response.json();
+
+      if (response.ok && data.status === "success") {
+        // เก็บ token ใน localStorage
+        localStorage.setItem('access_token', data.access_token);
+        console.log("Login successful!");
+        console.log("Token:", data.access_token);
+        navigate("/detail");
+      } else {
+        console.log("Login failed");
+      }
+
+
+
+    } catch (error) {
+      console.error("Error during login:", error);
+    }
+  };
+
+
+
+
+  const [formData, setFormData] = useState({
+    adminname: "",
+    password: ""
+  });
+  
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
@@ -39,10 +91,12 @@ function LoginPage() {
           <div className="flex flex-col w-full">
             <label htmlFor="email" className="mb-1 text-lg">Email</label>
             <input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="your@example.com"
+              type="text"
+              id="adminname"
+              name="adminname"
+              value={formData.adminname}
+              onChange={handleChange}
+              placeholder="Enter admin name"
               className="h-10 px-5 bg-gray-200 rounded-md border-none focus:outline-none"
             />
           </div>
@@ -53,6 +107,8 @@ function LoginPage() {
               type="password"
               id="password"
               name="password"
+              value={formData.password}
+              onChange={handleChange}
               placeholder="At least 8 characters"
               className="h-10 px-5 bg-gray-200 rounded-md border-none focus:outline-none"
             />
